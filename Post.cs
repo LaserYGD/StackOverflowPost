295ac6f0f40891de04e75911e1f7301b9b8c9fba
Artyom Volkov <tricky.fat.cat@gmail.com>
2019-11-11 02:34:34 +0300

    Added post class

---
+++ Post.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        private readonly DateTime _creationDate = new DateTime();
+        private int _votesCount = 0;
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+        }
+
+        public int Votes
+        {
+            get { return _votesCount; }
+        }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public void UpVote()
+        {
+            _votesCount++;
+        }
+
+        public void DownVote()
+        {
+            _votesCount--;
+        }
+    }
+}
